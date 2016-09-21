d8c7af33ca89a7b25a6681028b8e18a1053967b9
JorgeS45 <js1209119@gmail.com>
2016-09-20 22:31:07 -0700

    Signed-off-by: JorgeS45 <js1209119@gmail.com>

--- Hmwk/Ass1/Savitch_9thEd_Chap1_Prob2_CS/main.cpp
+++ Hmwk/Ass1/Savitch_9thEd_Chap1_Prob2_CS/main.cpp
@@ -2,7 +2,7 @@
    File:   main
    Author: Jorge Sanchez
    Created on September 20, 2016, 3:14 PM
-   Purpose:  
+   Purpose:  Make big CS
  */
 
 //System Libraries
@@ -25,25 +25,28 @@ int main(int argc, char** argv) {
     char i;    //Character used to make the !
     
     //Input values
-    cout<<"Choose a character to make the CS, input/n";
+    cout<<"Choose a character to make the CS"<<endl;
     cin>>a>>e>>i;
     //Process values -> Map inputs to Outputs
     
     //Display Output -> The Big C
-    cout<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<endl;
-    cout<<h<<h<<h<<h<<a<<a<<h<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<h<<a<<h<<h<<a<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<a<<h<<h<<h<<h<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<a<<h<<h<<h<<h<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<a<<h<<h<<h<<h<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<a<<h<<h<<h<<h<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<a<<h<<h<<h<<h<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<h<<a<<h<<h<<a<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<h<<h<<a<<a<<h<<h<<v<<v<<v<<v<<v<<v<<v<<endl;
-    cout<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<endl;
-    cout<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<endl;
-
+    cout<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<endl;
+    cout<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<endl;
+    cout<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<endl;
+    cout<<h<<h<<a<<a<<a<<a<<h<<h<<h<<h<<e<<e<<h<<h<<h<<i<<i<<endl;
+    cout<<h<<a<<h<<h<<h<<h<<a<<h<<h<<e<<h<<h<<e<<h<<h<<i<<i<<endl;
+    cout<<a<<h<<h<<h<<h<<h<<h<<h<<h<<e<<h<<h<<h<<h<<h<<i<<i<<endl;
+    cout<<a<<h<<h<<h<<h<<h<<h<<h<<h<<e<<h<<h<<h<<h<<h<<i<<i<<endl;
+    cout<<a<<h<<h<<h<<h<<h<<h<<h<<h<<h<<e<<h<<h<<h<<h<<i<<i<<endl;
+    cout<<a<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<e<<h<<h<<h<<i<<i<<endl;
+    cout<<a<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<e<<h<<h<<h<<h<<endl;
+    cout<<h<<a<<h<<h<<h<<h<<a<<h<<h<<e<<h<<h<<e<<h<<h<<i<<i<<endl;
+    cout<<h<<h<<a<<a<<a<<a<<h<<h<<h<<h<<e<<e<<e<<h<<h<<i<<i<<endl;
+    cout<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<endl;
+    cout<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<h<<endl;
+    cout<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<v<<endl;
+    
+    cout<<"Computer Science is cool stuff!!"<<endl;
     //Exit Program
     return 0;
     
